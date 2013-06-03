fa0e69ba8aa9bfc337ba15e8c768ecb819b3f634
user <user@user-PC>
2013-06-03 10:59:23 +0800

    Incomplete - task : install user management Fix use of $_POST in function handle AjaxRequest.

--- components/Relation.php
+++ components/Relation.php
@@ -720,8 +720,8 @@ echo '</ul>';
 					array('multiple' => 'multiple'));
 		}
 
-		public function handleAjaxRequest($_POST) {
-			print_r($_POST);
+		public function handleAjaxRequest($mypost) {
+			print_r($mypost);
 		}
 
 		public function renderTwoPaneSelection() 
